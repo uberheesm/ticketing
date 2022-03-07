72e8d5ede8ab32ae2d4fda0680c8c5daf02f3c29
Mike Lee <uberheesm@gmail.com>
2022-03-07 18:48:46 +0900

    currentUser route test file changed in auth server

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -25,13 +25,13 @@ it('responds with details about the current user', async () => {
     expect(response.body.currentUser.email).toEqual('test@test.com');
 });
 
-it('responds with null if not authenticated', async () => {
+it('responds with undefined if not authenticated', async () => {
     const response = await (
         request(app)
             .get('/api/users/currentUser')
             .send()
-            .expect(200)
+            .expect(401)
     );
 
-    expect(response.body.currentUser).toEqual(null);
+    expect(response.body.currentUser).toEqual(undefined);
 });
